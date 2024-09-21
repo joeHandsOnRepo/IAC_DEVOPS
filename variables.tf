10ad62ca8e2ae4e53dfd0ef42c7aba9e2f9a9349
joeHandsOnRepo <74275293+joeHandsOnRepo@users.noreply.github.com>
2024-09-20 20:10:59 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,21 @@
+variable "ResourceGroupName" {
+    type = string
+    default = "IAC-VM-FN"
+    }
+  
+variable "subscription_id" {
+  description = "The Subscription ID where Azure resources will be created"
+}
+
+variable "client_id" {
+  description = "The Client ID (Application ID) for the Service Principal"
+}
+
+variable "client_secret" {
+  description = "The Client Secret for the Service Principal"
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID associated with your Azure Active Directory"
+}
 
