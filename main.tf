faa5dba6e4de33112e445ecf3c0d747100d7c3a2
joeHandsOnRepo <74275293+joeHandsOnRepo@users.noreply.github.com>
2024-09-20 20:09:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-
+provider "azurerm" {
+ features{}
+  
+subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+resource "azurerm_resource_group" "Terraform_RG" {
+  name     = Terraform_RG
+  location = "East US"
+  }
